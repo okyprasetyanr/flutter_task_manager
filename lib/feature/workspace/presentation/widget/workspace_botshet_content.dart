70130b76a4df437209ecd24da4e6574eb30fe5f8
okyprasetyanr <okyprasetya89@gmail.com>
2026-06-24 21:34:05 +0700

    build Create, Update, Delete for Workspace in Workspace Feature

--- lib/feature/workspace/presentation/widget/workspace_botshet_content.dart
+++ lib/feature/workspace/presentation/widget/workspace_botshet_content.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:task_manager/core/app_properties/app_properties.dart';
+import 'package:task_manager/feature/shared_component/user/domain/model/model_user.dart';
 import 'package:task_manager/feature/workspace/domain/model/model_workspace_merge.dart';
 import 'package:task_manager/feature/workspace/presentation/bloc/workspace_bloc.dart';
 import 'package:task_manager/feature/workspace/presentation/bloc/workspace_event.dart';
 import 'package:task_manager/feature/workspace/presentation/bloc/workspace_state.dart';
+import 'package:task_manager/shared/common_widget/button/custom_button.dart';
 import 'package:task_manager/shared/enum/enum_status_state.dart';
+import 'package:task_manager/shared/style/icon_size.dart';
 import 'package:task_manager/shared/style/text_size.dart';
 import 'package:task_manager/shared/common_widget/button/custom_button_icon.dart';
 import 'package:task_manager/shared/common_widget/loading/custom_loading.dart';
@@ -25,6 +29,15 @@ class _WorkspaceBotshetContentState extends State<WorkspaceBotshetContent> {
   final descriptionController = TextEditingController();
   final _keyForm = GlobalKey<FormState>();
   bool _initialized = false;
+  final listUser = <ModelUser>{}.obs;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocListener<WorkspaceBloc, WorkspaceState>(
@@ -44,6 +57,7 @@ class _WorkspaceBotshetContentState extends State<WorkspaceBotshetContent> {
             _initialized = true;
             nameController.text = data.dataWorkspace.name;
             descriptionController.text = data.dataWorkspace.description;
+            listUser.addAll(data.dataWorkspaceMember);
           }
           return Padding(
             padding: const EdgeInsets.all(10),
@@ -51,31 +65,139 @@ class _WorkspaceBotshetContentState extends State<WorkspaceBotshetContent> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text("Form Workspace", style: titleTextStyle),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Form(
-                      key: _keyForm,
-                      child: ListView(
-                        controller: widget.scrollController,
-                        children: [
-                          const SizedBox(height: 15),
-                          CustomTextField(
-                            controller: nameController,
-                            label: "Name",
-                            validator: (value) =>
-                                value!.isEmpty ? "Name required!" : null,
-                          ),
-                          const SizedBox(height: 15),
-                          CustomTextField(
-                            controller: descriptionController,
-                            label: "Description",
-                            validator: (value) =>
-                                value!.isEmpty ? "Description required!" : null,
-                          ),
-                        ],
-                      ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Form(
+                    key: _keyForm,
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 15),
+                        CustomTextField(
+                          controller: nameController,
+                          label: "Name",
+                          validator: (value) =>
+                              value!.isEmpty ? "Name required!" : null,
+                        ),
+                        const SizedBox(height: 15),
+                        CustomTextField(
+                          controller: descriptionController,
+                          label: "Description",
+                          validator: (value) =>
+                              value!.isEmpty ? "Description required!" : null,
+                        ),
+                        const SizedBox(height: 10),
+                      ],
                     ),
+                  ),
+                ),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Text("List Member", style: lv2TextStyle),
+                            Expanded(
+                              child: Obx(() {
+                                return ListView.builder(
+                                  controller: widget.scrollController,
+                                  shrinkWrap: true,
+                                  itemCount: listUser.length,
+                                  itemBuilder: (context, index) {
+                                    final data = listUser.elementAt(index);
+                                    return Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          data.name,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        CustomButton(
+                                          backgroundColor:
+                                              AppPropertyColor.white,
+                                          onPressed: () => listUser.removeWhere(
+                                            (element) => element.id == data.id,
+                                          ),
+                                          child: Icon(
+                                            Icons.close_rounded,
+                                            color: AppPropertyColor.red,
+                                          ),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              }),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child:
+                            BlocSelector<
+                              WorkspaceBloc,
+                              WorkspaceState,
+                              Set<ModelUser>
+                            >(
+                              selector: (state) => state is WorkspaceStateLoaded
+                                  ? state.dataUser
+                                  : {},
+                              builder: (context, state) => Column(
+                                children: [
+                                  Text("List Member", style: lv2TextStyle),
+                                  Expanded(
+                                    child: ListView.builder(
+                                      controller: widget.scrollController,
+                                      shrinkWrap: true,
+                                      itemCount: state.length,
+                                      itemBuilder: (context, index) {
+                                        final data = state.elementAt(index);
+                                        return Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Text(
+                                              data.name,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                            Obx(
+                                              () => CustomButton(
+                                                backgroundColor:
+                                                    AppPropertyColor.white,
+                                                onPressed: () =>
+                                                    listUser.add(data),
+                                                child:
+                                                    listUser.any(
+                                                      (element) =>
+                                                          element.id == data.id,
+                                                    )
+                                                    ? Icon(
+                                                        Icons
+                                                            .check_circle_outline_rounded,
+                                                        size: lv2IconSize,
+                                                        color: AppPropertyColor
+                                                            .primary,
+                                                      )
+                                                    : Icon(
+                                                        Icons
+                                                            .radio_button_unchecked_rounded,
+                                                        size: lv2IconSize,
+                                                      ),
+                                              ),
+                                            ),
+                                          ],
+                                        );
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                      ),
+                    ],
                   ),
                 ),
                 const SizedBox(height: 10),
@@ -83,70 +205,68 @@ class _WorkspaceBotshetContentState extends State<WorkspaceBotshetContent> {
                   selector: (state) => state is WorkspaceStateLoaded
                       ? state.status
                       : EnumStatusState.none,
-                  builder: (context, state) => Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: state == EnumStatusState.synchronize
-                        ? const CustomLoading()
-                        : Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              if (data != null)
-                                Expanded(
-                                  child: CustomButtonIcon(
-                                    icon: Icon(
-                                      Icons.delete_rounded,
-                                      color: AppPropertyColor.white,
-                                    ),
-                                    backgroundColor: AppPropertyColor.red,
-                                    label: Text(
-                                      "Delete",
-                                      style: lv1TextStyleWhite,
-                                    ),
-                                    padding: true,
-                                    onPressed: () => context
-                                        .read<WorkspaceBloc>()
-                                        .add(WorkspaceEventDeleteWorkspace()),
-                                  ),
-                                ),
+                  builder: (context, state) =>
+                      state == EnumStatusState.synchronize
+                      ? const CustomLoading()
+                      : Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            if (data != null)
                               Expanded(
                                 child: CustomButtonIcon(
                                   icon: Icon(
-                                    Icons.check_rounded,
+                                    Icons.delete_rounded,
                                     color: AppPropertyColor.white,
                                   ),
-                                  backgroundColor: AppPropertyColor.primary,
+                                  backgroundColor: AppPropertyColor.red,
                                   label: Text(
-                                    data != null ? "Update" : "Add",
+                                    "Delete",
                                     style: lv1TextStyleWhite,
                                   ),
                                   padding: true,
-                                  onPressed: () {
-                                    if (!_keyForm.currentState!.validate()) {
-                                      return;
-                                    }
-                                    data != null
-                                        ? context.read<WorkspaceBloc>().add(
-                                            WorkspaceEventUpdateWorkspace(
-                                              name: nameController.text,
-                                              description:
-                                                  descriptionController.text,
-                                              contributor: {},
-                                            ),
-                                          )
-                                        : context.read<WorkspaceBloc>().add(
-                                            WorkspaceEventCreateWorkspace(
-                                              name: nameController.text,
-                                              description:
-                                                  descriptionController.text,
-                                              contributor: {},
-                                            ),
-                                          );
-                                  },
+                                  onPressed: () => context
+                                      .read<WorkspaceBloc>()
+                                      .add(WorkspaceEventDeleteWorkspace()),
                                 ),
                               ),
-                            ],
-                          ),
-                  ),
+                            Expanded(
+                              child: CustomButtonIcon(
+                                icon: Icon(
+                                  Icons.check_rounded,
+                                  color: AppPropertyColor.white,
+                                ),
+                                backgroundColor: AppPropertyColor.primary,
+                                label: Text(
+                                  data != null ? "Update" : "Add",
+                                  style: lv1TextStyleWhite,
+                                ),
+                                padding: true,
+                                onPressed: () {
+                                  if (!_keyForm.currentState!.validate()) {
+                                    return;
+                                  }
+                                  data != null
+                                      ? context.read<WorkspaceBloc>().add(
+                                          WorkspaceEventUpdateWorkspace(
+                                            name: nameController.text,
+                                            description:
+                                                descriptionController.text,
+                                            contributor: {},
+                                          ),
+                                        )
+                                      : context.read<WorkspaceBloc>().add(
+                                          WorkspaceEventCreateWorkspace(
+                                            name: nameController.text,
+                                            description:
+                                                descriptionController.text,
+                                            contributor: {},
+                                          ),
+                                        );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
                 ),
               ],
             ),
