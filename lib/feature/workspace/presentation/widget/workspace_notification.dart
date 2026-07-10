be0e7c4f3fe794ee498bc44cacf75fa9ccb795db
okyprasetyanr <okyprasetya89@gmail.com>
2026-07-10 15:56:54 +0700

    fix: polish UI for notification Summary

--- lib/feature/workspace/presentation/widget/workspace_notification.dart
+++ lib/feature/workspace/presentation/widget/workspace_notification.dart
@@ -12,58 +12,87 @@ class WorkspaceNotification extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 40,
-      child:
-          BlocSelector<WorkspaceBloc, WorkspaceState, Set<ModelNotification>>(
-            selector: (state) =>
-                state is WorkspaceStateLoaded ? state.dataNotification : {},
-            builder: (context, state) {
-              final newNotification = state.where((element) => !element.isRead);
-              if (state.isEmpty) {
-                return Text(
-                  "You don't have any notifications.",
-                  style: lv1TextStyleWhite,
+      child: Center(
+        child:
+            BlocSelector<WorkspaceBloc, WorkspaceState, Set<ModelNotification>>(
+              selector: (state) =>
+                  state is WorkspaceStateLoaded ? state.dataNotification : {},
+              builder: (context, state) {
+                final newNotification = state.where(
+                  (element) => !element.isRead,
                 );
-              }
-              if (state.isNotEmpty && state.any((element) => !element.isRead)) {
-                return Column(
-                  children: [
-                    Text.rich(
-                      TextSpan(
-                        children: [
-                          TextSpan(text: "You have ", style: lv1TextStyleWhite),
-                          TextSpan(
-                            text: newNotification.length.toString(),
-                            style: lv1TextStyleWhiteBold,
-                          ),
-                          TextSpan(
-                            text: " unread Notifications!",
-                            style: lv1TextStyleWhite,
-                          ),
-                        ],
+                final lastNotification = state.reduce((value, element) {
+                  DateTime currentStyle = value.createdAt;
+                  DateTime nextStyle = element.createdAt;
+                  return currentStyle.isAfter(nextStyle) ? value : element;
+                });
+                if (state.isEmpty) {
+                  return Text(
+                    "You don't have any notifications.",
+                    style: lv1TextStyleWhite,
+                  );
+                }
+                if (state.isNotEmpty &&
+                    state.any((element) => !element.isRead)) {
+                  return Column(
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "You have ",
+                              style: lv1TextStyleWhite,
+                            ),
+                            TextSpan(
+                              text: newNotification.length.toString(),
+                              style: lv1TextStyleWhiteBold,
+                            ),
+                            TextSpan(
+                              text: " unread Notifications!",
+                              style: lv1TextStyleWhite,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                    Text.rich(
-                      TextSpan(
-                        text: "Your latest notification is about: ",
-                        style: lv1TextStyleWhite,
-                        children: [
-                          TextSpan(
-                            text: newNotification.first.title,
-                            style: lv1TextStyleWhiteBold,
-                          ),
-                        ],
+                      Text.rich(
+                        TextSpan(
+                          text: "Your latest notification is about: ",
+                          style: lv1TextStyleWhite,
+                          children: [
+                            TextSpan(
+                              text: newNotification.first.title,
+                              style: lv1TextStyleWhiteBold,
+                            ),
+                          ],
+                        ),
                       ),
+                    ],
+                  );
+                } else {
+                  return Text.rich(
+                    textAlign: TextAlign.center,
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "No new notifications.\nThe ",
+                          style: lv1TextStyleWhite,
+                        ),
+                        TextSpan(text: "latest ", style: lv1TextStyleWhiteBold),
+                        TextSpan(
+                          text: "notification was about ",
+                          style: lv1TextStyleWhite,
+                        ),
+                        TextSpan(
+                          text: lastNotification.title,
+                          style: lv1TextStyleWhiteBold,
+                        ),
+                      ],
                     ),
-                  ],
-                );
-              } else {
-                return Text(
-                  "No new notifications.",
-                  style: lv3TextStyleWhiteBold,
-                );
-              }
-            },
-          ),
+                  );
+                }
+              },
+            ),
+      ),
     );
   }
 }
